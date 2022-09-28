d2aadabb0a16e3f4f2db592f220a4a886de3a7a3
Student <chadharris2002@gmail.com>
2022-09-28 10:05:11 -0400

    We did it

---
+++ scipt.js
@@ -0,0 +1,35 @@
+// 1
+// let const var
+// 2
+let num = 20;
+console.log(num);
+// 3
+num = 15;
+console.log(num);
+// 4
+num += 7;
+// num = num + 7
+console.log(num);
+// 5
+num--;
+console.log(num)
+// 6
+num = "this is not a number";
+console.log(num);
+// 7 8
+let blank;
+console.log(blank);
+
+//  9
+blank = null;
+console.log(blank);
+
+// bonus
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// const [quarter, dime, nickel, penny ] = [0.25, 0.10, 0.05, 0.01]
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+console.log(letters[letters.length - 1 ])
+// console.log(letters.length)
+// console.log(letters[9])
